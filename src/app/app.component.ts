0d98c1f148cbef2b1a3f56077c52dc4d6d0fdb9a
Samrath <samrathkumarmuz@gmail.com>
2020-06-25 18:18:25 +0530

    made a simple app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'First-Angular-app';
+  
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem(){
+     if(this.todoValue !== ""){
+       const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isdone: false
+      }
+      this.list.push(newItem);
+     }
+    this.todoValue = '';
+    // console.log(this.list[0].value)
+  }
+
+  deleteItem(id: number) {
+     this.list = this.list.filter(items => items.id !== id);
+  }
 }
